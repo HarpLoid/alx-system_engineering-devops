3011b851c97c2437ac2dc818f54344dcf4557c5b
HarpII <walesii19@gmail.com>
2023-03-30 12:46:34 +0000

    initial commit

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,20 @@
+# Using puppet to make changes to our configuration file
+# so that one can connect to a server without typing a password.
+
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/school$',
+  replace            => true,
+  append_on_no_match => true
+}
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
+}
