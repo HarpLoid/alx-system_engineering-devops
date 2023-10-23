b8f82e251ff107cf2edd390171924f73c43ef5ce
HarpLoid <walesii19@gmail.com>
2023-10-23 23:00:22 +0100

    export to csv and json

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,41 @@
 #!/usr/bin/python3
+"""
+Request from API; Return TODO list progress given employee ID
+Export this data to CSV
+"""
+import csv
+import requests
+from sys import argv
 
 
+def csv_export():
+    """
+    returns API data
+    """
+    USERNAME = ""
+    TASK_TITLE = []
 
+    users = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    user_json = users.json()
+    USERNAME = user_json.get('username')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    for todo in todos.json():
+        if todo.get('userId') == int(argv[1]):
+            TASK_TITLE.append((todo.get('title'), todo.get('completed')))
+
+    file_name = "{}.csv".format(user_json.get('id'))
+    with open(file_name, "w") as file:
+        fieldnames = ["USER_ID", "USERNAME",
+                      "TASK_COMPETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+        for task in TASK_TITLE:
+            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
+                             "TASK_COMPETED_STATUS": task[1],
+                             "TASK_TITLE": task[0]})
 
 
 if __name__ == "__main__":
-
+    csv_export()
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,7 +1,41 @@
 #!/usr/bin/python3
+"""
+Request from API; Return TODO list progress given employee ID
+Export this data to CSV
+"""
+import json
+import requests
+from sys import argv
 
 
+def json_export():
+    """
+    returns API data
+    """
+    USERNAME = ""
+    TASK_TITLE = []
 
+    users = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    user_json = users.json()
+    USERNAME = user_json.get('username')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    for todo in todos.json():
+        if todo.get('userId') == int(argv[1]):
+            TASK_TITLE.append((todo.get('title'), todo.get('completed')))
+
+    task = []
+    for tsk in TASK_TITLE:
+        task.append({"task": tsk[0],
+                     "completed": tsk[1],
+                     "username": USERNAME})
+
+    data = {str(argv[1]): task}
+    file_name = "{}.json".format(user_json.get('id'))
+    with open(file_name, "w") as file:
+        json.dump(data, file, indent=4)
 
 
 if __name__ == "__main__":
+    json_export()
