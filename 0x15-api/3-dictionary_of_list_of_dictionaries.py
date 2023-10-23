e88c3f61535db1842336549bba7603d8b9303639
HarpLoid <walesii19@gmail.com>
2023-10-23 23:25:43 +0100

    fixed typo error

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -28,14 +28,16 @@ def export_all():
     for user in USERS:
         task = []
         for tsk in TASK_TITLE:
-            task.append({"task": tsk[1],
-                        "completed": tsk[2],
-                         "username": user[1]})
+            task.append({"username": user[1],
+                         "task": tsk[1],
+                        "completed": tsk[2]})
         data[str(user[0])] = task
+
     file_name = "todo_all_employees.json"
+
     with open(file_name, "w") as file:
         json.dump(data, file,
-                  indent=4)
+                  indent=2)
 
 
 if __name__ == "__main__":
